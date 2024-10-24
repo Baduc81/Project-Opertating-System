d0de2dd1831617aeeab836de336362fcd35b5345
thienanta1501 <thienanpham1501@gmail.com>
2024-10-25 01:17:44 +0700

    Finish pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,25 +3,42 @@
 #include "user/user.h"
 
 int main() {
-  int p[2];
+  int p[2]; // pipe từ cha sang con
+  int q[2]; // pipe từ con sang cha
   char buf[100];
+  char byte;
 
-  // Tạo pipe
+  // Tạo 2 pipe
   pipe(p);
+  pipe(q);
 
   // Tạo tiến trình con
   int pid = fork();
-  if (pid == 0) {
+
+  if (pid < 0) {
+    // Nếu fork thất bại
+    printf("Fork failed!\n");
+    exit(1);
+  }
+  else if (pid == 0) {
     // Tiến trình con: gửi tin nhắn cho tiến trình cha
-    write(p[1], "ping", 4);  // Ghi "ping" vào pipe
-    printf("Thread id %d: received ping\n", getpid());
+    // Đọc từ ống dẫn p (cha sang con)
+    read(p[0], &buf, 1);
+    // In ra thông báo
+    printf("%d: received ping\n", getpid());
+    // Ghi byte lên ống dẫn q (con sang cha)
+    write(q[1], &buf, 1);
+    // Kết thúc tiến trình con
+    exit(0);
   } 
   else {
     // Tiến trình cha: đợi tiến trình con, sau đó đọc tin nhắn
-    wait(0);  // Đợi tiến trình con kết thúc
-    read(p[0], buf, 4);  // Đọc "ping" từ pipe
-    printf("Thread id %d: received pong\n", getpid());
+    write(p[1], &byte, 1);
+    // Đợi tiến trình con kết thúc và đọc từ ống dẫn q (con sang cha)
+    read(q[0], &buf, 1);
+    // In ra thông báo
+    printf("%d: received pong\n", getpid());
+    // Kết thúc tiến trình cha
+    exit(0);
   }
-
-  return 0;
 }
