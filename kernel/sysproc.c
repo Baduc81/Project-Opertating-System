895ce5ffe5cc3df45aa55d6ce2313f7197e73578
nguyenmy2402 <myn047450@gmail.com>
2024-11-23 17:20:12 +0700

    testlab2

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,20 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void)
+{
+  int trace_mask;
+  argint(0,&trace_mask);
+
+  // argint failed
+  if(trace_mask < 0){
+    return -1; 
+  }
+
+  struct proc*p=myproc();
+  p->traced = trace_mask;
+	return 0;
+
+}
