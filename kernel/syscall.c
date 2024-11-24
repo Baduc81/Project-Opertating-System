37d31e1df8d30c2904e81c9591ade778020e5605
nguyenmy2402 <myn047450@gmail.com>
2024-11-24 11:41:09 +0700

    Xoa ham syscallname bi thua

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -132,32 +132,6 @@ static uint64 (*syscalls[])(void) = {
 [SYS_sysinfo] sys_sysinfo,
 };
 
-char *syscallnames[] = {
-[SYS_fork]    "fork",
-[SYS_exit]    "exit",
-[SYS_wait]    "wait",
-[SYS_pipe]    "pipe",
-[SYS_read]    "read",
-[SYS_kill]    "kill",
-[SYS_exec]    "exec",
-[SYS_fstat]   "fstat",
-[SYS_chdir]   "chdir",
-[SYS_dup]     "dup",
-[SYS_getpid]  "getpid",
-[SYS_sbrk]    "sbrk",
-[SYS_sleep]   "sleep",
-[SYS_uptime]  "uptime",
-[SYS_open]    "open",
-[SYS_write]   "write",
-[SYS_mknod]   "mknod",
-[SYS_unlink]  "unlink",
-[SYS_link]    "link",
-[SYS_mkdir]   "mkdir",
-[SYS_close]   "close",
-[SYS_trace]   "trace",
-// [SYS_sysinfo] "sysinfo",
-};
-
 char* syscall_name(int syscall_num) {
     switch(syscall_num) {
     case SYS_fork: return "fork";
