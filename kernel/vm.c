fcd0c4f493742f59b511f2c6b12075e8aa874131
Baduc81 <baduc81@gmail.com>
2024-12-27 17:01:35 +0000

    Hoan thanh bai2

--- kernel/vm.c
+++ kernel/vm.c
@@ -488,9 +488,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 
 #ifdef LAB_PGTBL
+uint64 deepv = 0;
 void
 vmprint(pagetable_t pagetable) {
   // your code here
+    if (deepv == 0){
+    printf("page table %p \n", (void*)pagetable);
+  }
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      for (int j = 0; j <= deepv; j++){
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n", i, (void*)pte, (void*)PTE2PA(pte));
+    }
+    
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      deepv += 1;
+      vmprint((pagetable_t)child);
+      // vmprint((pagetable_t)PA2PVA(child));
+      deepv -= 1;
+    } 
+  }
 }
 #endif
 
